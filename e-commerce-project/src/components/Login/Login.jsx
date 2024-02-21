69084078b941e11ddf5fb60c97dba2a2ed85083d
Oguz Cinar <marazoguz_29@hotmail.com>
2024-02-21 16:12:34 +0300

    2o3kko

--- e-commerce-project/src/components/Login/Login.jsx
+++ e-commerce-project/src/components/Login/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useData } from "../Context/DataContext";
 
@@ -6,26 +6,11 @@ export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const { setExitClicked } = useData();
-
-  const getLogin = (email, password) => {
-    fetch("/https://post-app-ab2l.onrender.com/api/get-user-info", {
-      method: "POST",
-      headers: {
-        "Contact-type": "application/json",
-      },
-      body: JSON.stringify({ email: email, password: password }),
-    })
-      .then((response) => response.json())
-      .then((cas) => console.log(cas));
-  };
-
   return (
     <div className="form-container">
       <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          getLogin(email, password);
-        }}
+        action="https://post-app-ab2l.onrender.com/api/get-user-info"
+        method="post"
         className="form"
       >
         <div className="box">
